60b25bd2a47e2300a63a9ac2ad8b515ffa65dd49
imanderson588 <123431798+imanderson588@users.noreply.github.com>
2026-06-05 09:54:51 -0600

    Extend apply_stress.sh stress loop to 550

--- TaC_ACE/PeierlsStress/apply_stress.sh
+++ TaC_ACE/PeierlsStress/apply_stress.sh
@@ -1,7 +1,7 @@
 rm energy.yaml
 rm -f peierlsstress.out
 
-for i in    80 100 120 140 160 180 200 225 250 275 300 ;
+for i in    80 100 120 140 160 180 200 225 250 275 300 325 350 375 400 425 450 475 500 525 550 ;
 do
 
    mpirun -np 64 lmp -in applystress.in -var stress $i >> peierlsstress.out
