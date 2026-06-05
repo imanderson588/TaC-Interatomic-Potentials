352901a46bce0ab2fe992aa24b8f6dae4063f10f
imanderson588 <123431798+imanderson588@users.noreply.github.com>
2026-06-04 18:00:40 -0600

    Redirect apply_stress.sh LAMMPS output to peierlsstress.out

--- TaC_SNAP/PeierlsStress/apply_stress.sh
+++ TaC_SNAP/PeierlsStress/apply_stress.sh
@@ -1,8 +1,9 @@
 rm energy.yaml
+rm -f peierlsstress.out
 
 for i in    80 100 120 140 160 180 200 225 250 275 300 ;
 do
 
-   mpirun -np 64 lmp -in applystress.in -var stress $i
+   mpirun -np 64 lmp -in applystress.in -var stress $i >> peierlsstress.out
 
 done
