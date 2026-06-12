fb66a86af9d0c1fabe0a8de525c2a5550be8b421
Ian <ianm.andeerson@colostate.edu>
2026-06-12 11:33:26 -0600

    Initial script tests

--- DislocationPosition/calcDislocPosition.py
+++ DislocationPosition/calcDislocPosition.py
@@ -1,5 +1,9 @@
-from ovito import import_file
-from ovito.modifiers import SelectTypeModifier, CommonNeighborAnalysisModifier, DeleteSelectedModifier
+from ovito.io import import_file, export_file
+from ovito.modifiers import SelectTypeModifier, CommonNeighborAnalysisModifier, DeleteSelectedModifier, ExpressionSelectionModifier
+import warnings
+warnings.filterwarnings('ignore', message='.*OVITO.*PyPI')
+import ovito._extensions.pyscript
+
 
 """
 This file uses the Ovito python API to locate and output the position of an edge dislocation within the simulation cell.
@@ -7,15 +11,32 @@ This is accomplished by first deleting all carbon atoms, then delecting all atom
 in the center of the simulation cell and calculating the average y-position of the remaining atoms.
 """
 
-pipeline = import_file("Dump1")
+pipeline = import_file("dump.652")
 
 
-pipeline.modifiers.append(SelectTypeModifier(operate_on="particles", property="Particle Type", types = {"C"} ))
+pipeline.modifiers.append(SelectTypeModifier(operate_on="particles", property="Particle Type", types = {"Type 2"} ))
 
-pipeline.modifiers.append(DeleteSelectedModifier(operate_on="particles"))
+pipeline.modifiers.append(DeleteSelectedModifier())
 
 pipeline.modifiers.append(CommonNeighborAnalysisModifier())
 
 pipeline.modifiers.append
 
+data = pipeline.compute()
 
+z_coordinates = data.particles['Position'][:,2]
+
+z_min = min(z_coordinates)
+z_max = max(z_coordinates)
+
+surface_cutoff = 5
+
+expression = f"Position.Z < {z_min + surface_cutoff} || Position.Z > {z_max - surface_cutoff}"
+pipeline.modifiers.append(ExpressionSelectionModifier(expression=expression))
+
+pipeline.modifiers.append(DeleteSelectedModifier())
+
+final_data = pipeline.compute()
+
+
+export_file(pipeline, "output_cleaned.dump", format="lammps/dump", columns = ["Position.X", "Position.Y", "Position.Z"])
