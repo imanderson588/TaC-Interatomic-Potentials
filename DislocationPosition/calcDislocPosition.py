72cfa0409535718fef5ba4308db71e2f6b3ed55e
Ian <ianm.andeerson@colostate.edu>
2026-06-12 13:39:51 -0600

    file updates

--- DislocationPosition/calcDislocPosition.py
+++ DislocationPosition/calcDislocPosition.py
@@ -1,8 +1,5 @@
 from ovito.io import import_file, export_file
 from ovito.modifiers import SelectTypeModifier, CommonNeighborAnalysisModifier, DeleteSelectedModifier, ExpressionSelectionModifier
-import warnings
-warnings.filterwarnings('ignore', message='.*OVITO.*PyPI')
-import ovito._extensions.pyscript
 
 
 """
@@ -10,37 +7,40 @@ This file uses the Ovito python API to locate and output the position of an edge
 This is accomplished by first deleting all carbon atoms, then delecting all atoms in the FCC structure, then selecting atoms
 in the center of the simulation cell and calculating the average y-position of the remaining atoms.
 """
-
-pipeline = import_file("dump.652")
-
-
-pipeline.modifiers.append(SelectTypeModifier(operate_on="particles", property="Particle Type", types = {"Type 2"} ))
-
-pipeline.modifiers.append(DeleteSelectedModifier())
-
-data = pipeline.compute()
-
-pipeline.modifiers.append(CommonNeighborAnalysisModifier())
-
-pipeline.modifiers.append(SelectTypeModifier(operate_on='particles',property='Structure Type', types={"FCC"}))
-
-pipeline.modifiers.append(DeleteSelectedModifier())
-
-data = pipeline.compute()
-
-z_coordinates = data.particles['Position'][:,2]
-
-z_min = min(z_coordinates)
-z_max = max(z_coordinates)
-
-surface_cutoff = 5
-
-expression = f"Position.Z < {z_min + surface_cutoff} || Position.Z > {z_max - surface_cutoff}"
-pipeline.modifiers.append(ExpressionSelectionModifier(expression=expression))
-
-pipeline.modifiers.append(DeleteSelectedModifier())
-
-final_data = pipeline.compute()
+def calculate_average_position(input_file):
+    pipeline = import_file("dump.652")
 
 
-export_file(pipeline, "output_cleaned.dump", format="lammps/dump", columns = ["Position.X", "Position.Y", "Position.Z"])
+    pipeline.modifiers.append(SelectTypeModifier(operate_on="particles", property="Particle Type", types = {"Type 2"} ))
+
+    pipeline.modifiers.append(DeleteSelectedModifier())
+
+    data = pipeline.compute()
+
+    pipeline.modifiers.append(CommonNeighborAnalysisModifier())
+
+    pipeline.modifiers.append(SelectTypeModifier(operate_on='particles',property='Structure Type', types={"FCC"}))
+
+    pipeline.modifiers.append(DeleteSelectedModifier())
+
+    data = pipeline.compute()
+
+    z_coordinates = data.particles['Position'][:,2]
+
+    z_min = min(z_coordinates)
+    z_max = max(z_coordinates)
+
+    surface_cutoff = 5
+
+    expression = f"Position.Z < {z_min + surface_cutoff} || Position.Z > {z_max - surface_cutoff}"
+    pipeline.modifiers.append(ExpressionSelectionModifier(expression=expression))
+
+    pipeline.modifiers.append(DeleteSelectedModifier())
+
+    final_data = pipeline.compute()
+
+    y_coordinartes = data.particles['Position'][:,1]
+
+    average_y_position = sum(y_coordinartes)/len(y_coordinartes)
+
+    return average_y_position
